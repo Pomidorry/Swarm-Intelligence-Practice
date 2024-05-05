c92021eeec33773d9247039def6454d73376f633
Pomidorry <106489913+Pomidorry@users.noreply.github.com>
2024-05-05 19:06:56 +0300

    lab6cmpl

--- lab6SI/DEalg.py
+++ lab6SI/DEalg.py
@@ -6,14 +6,15 @@ from numpy import clip
 from numpy import argmin
 from numpy import min
 from numpy import around
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+#import tensorflow as tf
 
 def load_data():
     # Завантаження даних з xlsx-файлу
     global Data, xTrain, yTrain, xTest, yTest, x, y
-    Data = pd.read_excel("E:\\SI\\lab6SI\\DataRegression.xlsx", sheet_name="Var09")
+    Data = pd.read_excel("E:\\SI\\lab6SI\\DataRegression.xlsx", sheet_name="Var01")
     
     # Перемішування рядків з даними, що отримані з xlsx-файлу
     Data = Data.sample(frac=1).reset_index(drop=True)
@@ -28,88 +29,15 @@ def load_data():
     xTrain, xTest = x[:LenDataTrain], x[LenDataTrain:]
     yTrain, yTest = y[:LenDataTrain], y[LenDataTrain:]
 
+load_data()
 
-
-def subtract_lists(list1, list2):
-    return [item for item in list1 if item not in list2]
-
-def lossfunc(Y, y):
-    M = len(Y)  # Кількість стовпців у матриці Y
-    N = len(y)     # Кількість елементів у векторі y
-
-    # Повторюємо вектор y, щоб він мав таку ж форму, як матриця Y
-
-    # Обчислюємо z
-    Y1=sorted(Y)
-    Y1=np.array(Y1)
-    y1=sorted(y)
-    y1=np.array(y1)
-    z = np.sqrt((Y1 - y1)**2 / N)
-
-    return z
-
-
-def visualization():
-    global Data, b_best, xTrain, yTrain, xTest, yTest, MaxIter, GlobalBestFObj, LossFuncFObjInTest
-
-    # Отримання мінімального та максимального значення x
-    xMin = np.min(Data.iloc[:, 1])
-    xMax = np.max(Data.iloc[:, 1])
-
-    # Кількість точок для візуалізації
-    N = len(Data.iloc[:, 1])
-    h = (xMax - xMin) / (N - 1)
-    X = np.linspace(xMin, xMax, N)
-
-    # Обчислення Y за найкращими параметрами моделі
-    Y = model(b_best, X)
-    print(Y)
-
-    # Візуалізація
-    plt.figure(1)
-    plt.plot(Data.iloc[:, 1], Data.iloc[:, 0], '.', X, Y)
-    plt.grid(True)
-    plt.xlabel('X')
-    plt.ylabel('Y (Real, Model)')
-    plt.legend(['Real Data Y', 'Model Data Y'])
-
-    plt.figure(2)
-
-# Задання кількості ітерацій
-    it = np.arange(1, MaxIter)[::3]
-    y=Data.iloc[:, 0]
-    print(y.tolist())
-    LossFuncFObjInTest = lossfunc(Y, y.tolist())
-    print(LossFuncFObjInTest)
-    # Побудова підграфіків
-    # plt.subplot(2, 2, 1)
-    # plt.plot(it, GlobalBestFObj)
-    # plt.grid(True)
-    # plt.xlabel('Iterations')
-    # plt.ylabel('GlobalBestFObj')
-
-    plt.subplot(2, 2, 2)
-    plt.plot(it, LossFuncFObjInTest)
-    plt.grid(True)
-    plt.xlabel('Iterations')
-    plt.ylabel('LossFuncFObjInTest')
-
-    # plt.subplot(2, 1, 2)
-    # plt.plot(it, GlobalBestFObj, it, LossFuncFObjInTest)
-    # plt.grid(True)
-    # plt.xlabel('Iterations')
-    # plt.ylabel('FuncFObj (Train, Test)')
-    # plt.legend(['Train', 'Test'])
-    plt.show()
-
-
-
+y_f=y.tolist()
+x_f=x.tolist()
 # define objective function
-def obj(x, y, b_pop):
-    J = sum(np.power((y - (b_pop[0] + b_pop[1]*np.exp((-1)*x*b_pop[3])+b_pop[2]*np.exp((-1)*x*b_pop[4]))), 2))
-    return J, b_pop[0], b_pop[1]
+def obj(b):
+    b1, b2 = b
+    return sum([(y_f[i] - b1*(1-np.exp(-b2*x_f[i])))**2 for i in range(len(x_f))])
 
-#y=b1(1-e^(-b2*x))
 # define mutation operation
 def mutation(x, F):
     return x[0] + F * (x[1] - x[2])
@@ -130,18 +58,14 @@ def crossover(mutated, target, dims, cr):
     return trial
 
 
-def differential_evolution(xTrain, yTrain, pop_size, pop, bounds, iter, F, cr):
+def differential_evolution(pop_size, bounds, iter, F, cr):
     # initialise population of candidate solutions randomly within the specified bounds
-    
+    pop = bounds[:, 0] + (rand(pop_size, len(bounds)) * (bounds[:, 1] - bounds[:, 0]))
     # evaluate initial population of candidate solutions
-    all_values = [obj(xTrain, yTrain, ind) for ind in pop]
-    #print(objective_values)
+    obj_all = [obj(ind) for ind in pop]
     # find the best performing vector of initial population
-    objective_values = [item[0] for item in all_values]
-    #print(objective_values)
-    best_obj = min(objective_values)
-    #print(best_obj)
-    #print(objective_values[:][:][0])
+    best_vector = pop[argmin(obj_all)]
+    best_obj = min(obj_all)
     prev_obj = best_obj
     # run iterations of the algorithm
     for i in range(iter):
@@ -157,65 +81,122 @@ def differential_evolution(xTrain, yTrain, pop_size, pop, bounds, iter, F, cr):
             # perform crossover
             trial = crossover(mutated, pop[j], len(bounds), cr)
             # compute objective function value for target vector
-            obj_target = obj(xTrain, yTrain, pop[j])
-            #print(obj_target)
-            #obj_target = [item[0] for item in obj_target1]
+            obj_target = obj(pop[j])
             # compute objective function value for trial vector
-            obj_trial = obj(xTrain, yTrain, trial)
-            #obj_trial = [item[0] for item in obj_trial1]
+            obj_trial = obj(trial)
             # perform selection
-            if obj_trial[0] < obj_target[0]:
+            if obj_trial < obj_target:
                 # replace the target vector with the trial vector
                 pop[j] = trial
                 # store the new objective function value
-                objective_values[j] = obj_trial[0]
+                obj_all[j] = obj_trial
         # find the best performing vector at each iteration
-        #print(objective_values)
-        best_obj = min(objective_values)
-        #print(best_obj)
+        best_obj = min(obj_all)
         # store the lowest objective function value
         if best_obj < prev_obj:
+            best_vector = pop[argmin(obj_all)]
             prev_obj = best_obj
             # report progress at each iteration
-            print('Iteration: %d f([%s]) = %.5f' % (i, around(pop[argmin(objective_values)], decimals=5), best_obj))
-    return [pop[argmin(objective_values)], best_obj]
+            print('Iteration: %d f([%s]) = %.5f' % (i, around(best_vector, decimals=5), best_obj))
+    return best_vector, best_obj
 
-global Data, xTrain, yTrain, xTest, yTest, x, y
-
-# define population size
-pop_size = 150
-# define lower and upper bounds for every dimension
-# define number of iterations
-MaxIter = 100
-# define scale factor for mutation
-F = 0.5
-# define crossover rate for recombination
-cr = 0.7
-load_data()
-
-x_arr = np.array(x)
-y_arr = np.array(y)
-bounds = asarray([(0, 10), (0, 5), (-2, 2), (-2, 2), (-2, 2)])
-#print(xTrain)
-# perform differential evolution
-pop = bounds[:, 0] + (rand(pop_size, len(bounds)) * (bounds[:, 1] - bounds[:, 0]))
-
-solution = differential_evolution(x_arr, y_arr, pop_size, pop, bounds, MaxIter, F, cr)
-print(solution)
 
 
 def model(b, x):
     # Кількість точок x та кількість параметрів b
     N = len(x)
     M = len(b)
+
     # Ініціалізуємо матрицю Y з нулями
     Y = np.zeros((N, M))
+    print(b)
     # Обчислюємо Y для кожного параметра b
-    for i in range(M):
-        Y[:,i] = b[0] + b[1]*np.exp(-x*b[3])+b[2]*np.exp(-x*b[4])
-    return Y[:, 0]
-tem=solution[0]
-print(tem)
-b_best = tem.tolist()
-print(b_best)
-visualization()
+    Y = b[0] * (1 - np.exp(-b[1] * x))
+
+    return Y
+
+# def f(Y, y):
+#     #M = len(Y)  # Кількість стовпців у матриці Y
+#     N = len(y)     # Кількість елементів у векторі y
+
+#     # Повторюємо вектор y, щоб він мав таку ж форму, як матриця Y
+#     #y = np.tile(y, (1, M))
+#     loss_list=[]
+#     # Обчислюємо z
+#     for i in range(2):
+#         z = np.sqrt((Y[i] - y[i])**2) / N
+#         loss_list.append(z)
+
+#     return loss_list
+
+def mse_loss(y_true, y_pred):
+  """Calculates Mean Squared Error"""
+  return (((np.array(y_true) - np.array(y_pred)) ** 2))/len(y_true)
+
+
+def func(x, b1, b2):
+    return b1 * (1 - np.exp(-b2 * x))
+
+# define population size
+pop_size = 100
+# define lower and upper bounds for every dimension
+bounds = asarray([(0, 100), (0, 1)])
+# define number of iterations
+iter = 100
+# define scale factor for mutation
+F = 0.5
+# define crossover rate for recombination
+cr = 0.7
+ 
+# perform differential evolution
+solution = differential_evolution(pop_size, bounds, iter, F, cr)
+print(solution)
+
+all_b=solution[0]
+b_list=all_b.tolist()
+b1, b2 = b_list[0], b_list[1]
+
+x_values = np.linspace(0, max(x_f), 100)
+y_values_func = func(x_values, b1, b2)
+
+plt.scatter(x_f, y_f, label='Data')
+plt.plot(x_values, y_values_func, color='red', label='Function: y = b1(1 - exp(-b2*x))')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Regression plot')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+x_values_loss = np.linspace(0, max(x_f), 14)
+y_values_func_loss = func(x_values_loss, b1, b2)
+
+mse_losses=mse_loss(y_values_func_loss.tolist(), y_f)
+plt.plot(y_values_func_loss, mse_losses, label='MSE')
+plt.xlabel('Predicted Value (y_pred)')
+plt.ylabel('Loss')
+plt.show()
+YModelTrain = model(b_list, xTrain)
+
+# Получение предсказанных значений для тестового набора данных
+YModelTest = model(b_list, xTest)
+
+plt.plot(YModelTrain, yTrain, '.r', markersize=15)
+plt.plot(np.arange(min(yTrain), max(yTrain), 0.03), np.arange(min(yTrain), max(yTrain), 0.03))
+plt.xlabel('YModelTrain')
+plt.ylabel('yTrain (Real Data)')
+plt.grid(True)
+#print(YModelTrain)
+plt.axis([min(YModelTrain), max(YModelTrain), min(yTrain), max(yTrain)])
+plt.show()
+# График для тестовых данных
+
+plt.plot(YModelTest, yTest, '.', markersize=15)
+plt.plot(np.arange(min(yTest), max(yTest), 0.01), np.arange(min(yTest), max(yTest), 0.01))
+plt.xlabel('YModelTest')
+plt.ylabel('yTest (Real Data)')
+plt.grid(True)
+plt.axis([min(YModelTest), max(YModelTest), min(yTest), max(yTest)])
+plt.show()
+
+
---
+++ lab6SI/test.py
@@ -0,0 +1,285 @@
+# differential evolution search of the two-dimensional sphere objective function
+from numpy.random import rand
+from numpy.random import choice
+from numpy import asarray
+from numpy import clip
+from numpy import argmin
+from numpy import min
+from numpy import around
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import tensorflow as tf
+
+def load_data():
+    # Завантаження даних з xlsx-файлу
+    global Data, xTrain, yTrain, xTest, yTest, x, y
+    Data = pd.read_excel("E:\\SI\\lab6SI\\DataRegression.xlsx", sheet_name="Var01")
+    
+    # Перемішування рядків з даними, що отримані з xlsx-файлу
+    Data = Data.sample(frac=1).reset_index(drop=True)
+    
+    # Розділення на ознаки (x) і цільову змінну (y)
+    x = Data.iloc[:, 1]  # Ознака - другий стовпець
+    y = Data.iloc[:, 0]  # Цільова змінна - перший стовпець
+    
+    # Розділення на навчальну та тестову вибірки
+    TrainPercent = 75
+    LenDataTrain = round(len(Data) * TrainPercent / 100)
+    xTrain, xTest = x[:LenDataTrain], x[LenDataTrain:]
+    yTrain, yTest = y[:LenDataTrain], y[LenDataTrain:]
+
+load_data()
+
+y_f=y.tolist()
+x_f=x.tolist()
+# define objective function
+def obj(b):
+    b1, b2 = b
+    return sum([(y_f[i] - b1*(1-np.exp(-b2*x_f[i])))**2 for i in range(len(x_f))])
+
+# define mutation operation
+def mutation(x, F):
+    return x[0] + F * (x[1] - x[2])
+
+
+# define boundary check operation
+def check_bounds(mutated, bounds):
+    mutated_bound = [clip(mutated[i], bounds[i, 0], bounds[i, 1]) for i in range(len(bounds))]
+    return mutated_bound
+
+
+# define crossover operation
+def crossover(mutated, target, dims, cr):
+    # generate a uniform random value for every dimension
+    p = rand(dims)
+    # generate trial vector by binomial crossover
+    trial = [mutated[i] if p[i] < cr else target[i] for i in range(dims)]
+    return trial
+
+def model(b, x):
+    # Кількість точок x та кількість параметрів b
+    N = len(x)
+    M = len(b)
+
+    # Ініціалізуємо матрицю Y з нулями
+    Y = np.zeros((N, M))
+    print(b)
+    # Обчислюємо Y для кожного параметра b
+    Y = b[0] * (1 - np.exp(-b[1] * x))
+
+    return Y
+
+def mse_loss(y_true, y_pred):
+  """Calculates Mean Squared Error"""
+  return (((np.array(y_true) - np.array(y_pred)) ** 2))/len(y_true)
+
+def func(x, b1, b2):
+    return b1 * (1 - np.exp(-b2 * x))
+
+def differential_evolution(pop_size, bounds, iter, F, cr, k):
+    # initialise population of candidate solutions randomly within the specified bounds
+    pop = bounds[:, 0] + (rand(pop_size, len(bounds)) * (bounds[:, 1] - bounds[:, 0]))
+    # evaluate initial population of candidate solutions
+    obj_all = [obj(ind) for ind in pop]
+    # find the best performing vector of initial population
+    best_vector = pop[argmin(obj_all)]
+    best_obj = min(obj_all)
+    prev_obj = best_obj
+    # run iterations of the algorithm
+    for i in range(iter):
+        # iterate over all candidate solutions
+        for j in range(pop_size):
+            # choose three candidates, a, b and c, that are not the current one
+            candidates = [candidate for candidate in range(pop_size) if candidate != j]
+            a, b, c = pop[choice(candidates, 3, replace=False)]
+            # perform mutation
+            mutated = mutation([a, b, c], F)
+            # check that lower and upper bounds are retained after mutation
+            mutated = check_bounds(mutated, bounds)
+            # perform crossover
+            trial = crossover(mutated, pop[j], len(bounds), cr)
+            # compute objective function value for target vector
+            obj_target = obj(pop[j])
+            # compute objective function value for trial vector
+            obj_trial = obj(trial)
+            # perform selection
+            if obj_trial < obj_target:
+                # replace the target vector with the trial vector
+                pop[j] = trial
+                # store the new objective function value
+                obj_all[j] = obj_trial
+        # find the best performing vector at each iteration
+        best_obj = min(obj_all)
+        # store the lowest objective function value
+        if best_obj < prev_obj:
+            best_vector = pop[argmin(obj_all)]
+            prev_obj = best_obj
+            # report progress at each iteration
+        print('Iteration: %d f([%s]) = %.5f' % (i, around(best_vector, decimals=5), best_obj))
+        if i % k == 0:  # Додано умову для відображення кожні k ітерацій
+            all_b = best_vector
+            b_list = all_b.tolist()
+            b1, b2 = b_list[0], b_list[1]
+
+            x_values = np.linspace(0, max(x_f), 100)
+            y_values_func = func(x_values, b1, b2)
+
+            plt.scatter(x_f, y_f, label='Data')
+            plt.plot(x_values, y_values_func, color='red', label='Function: y = b1(1 - exp(-b2*x))')
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.title('Regression plot')
+            plt.legend()
+            plt.grid(True)
+            plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+            plt.show()
+
+            x_values_loss = np.linspace(0, max(x_f), 14)
+            y_values_func_loss = func(x_values_loss, b1, b2)
+
+            mse_losses = mse_loss(y_values_func_loss.tolist(), y_f)
+            plt.plot(y_values_func_loss, mse_losses, label='MSE')
+            plt.xlabel('Predicted Value (y_pred)')
+            plt.ylabel('Loss')
+            plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+            plt.show()
+
+            YModelTrain = model(b_list, xTrain)
+
+            # Получение предсказанных значений для тестового набора данных
+            YModelTest = model(b_list, xTest)
+
+            plt.plot(YModelTrain, yTrain, '.r', markersize=15)
+            plt.plot(np.arange(min(yTrain), max(yTrain), 0.03), np.arange(min(yTrain), max(yTrain), 0.03))
+            plt.xlabel('YModelTrain')
+            plt.ylabel('yTrain (Real Data)')
+            plt.grid(True)
+            #print(YModelTrain)
+            plt.axis([min(YModelTrain), max(YModelTrain), min(yTrain), max(yTrain)])
+            plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+            plt.show()
+            # График для тестовых данных
+
+            plt.plot(YModelTest, yTest, '.', markersize=15)
+            plt.plot(np.arange(min(yTest), max(yTest), 0.03), np.arange(min(yTest), max(yTest), 0.03))
+            plt.xlabel('YModelTest')
+            plt.ylabel('yTest (Real Data)')
+            plt.grid(True)
+            plt.axis([min(YModelTest), max(YModelTest), min(yTest), max(yTest)])
+            plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+            plt.show()
+    return best_vector, best_obj
+
+def pso(cost_func, k=10, dim=2, num_particles=200, max_iter=50, w=0.5, c1=1, c2=2):
+    # Initialize particles and velocities
+    #particles = np.random.uniform(-5.12, 5.12, (num_particles, dim))
+    dim_ranges = asarray([(0, 100), (0, 1)])  # Default ranges for 2 dimensions
+    particles = dim_ranges[:, 0] + (rand(num_particles, len(dim_ranges)) * (dim_ranges[:, 1] - dim_ranges[:, 0]))
+    velocities = np.zeros((num_particles, dim))
+    #velocities = np.zeros((num_particles, dim))
+
+    # Initialize the best positions and fitness values
+    best_positions = np.copy(particles)
+    best_fitness = np.array([cost_func(p) for p in particles])
+    swarm_best_position = best_positions[np.argmin(best_fitness)]
+    swarm_best_fitness = np.min(best_fitness)
+    min_values = np.array([0, 0])
+    max_values = np.array([100, 1])
+    # Iterate through the specified number of iterations, updating the velocity and position of each particle at each iteration
+    for i in range(max_iter):
+        # Update velocities
+        r1 = np.random.uniform(0, 1, (num_particles, dim))
+        r2 = np.random.uniform(0, 1, (num_particles, dim))
+        velocities = w * velocities + c1 * r1 * (best_positions - particles) + c2 * r2 * (swarm_best_position - particles)
+
+        # Update positions
+        particles += velocities
+        for d in range(dim):
+            particles[:, d] = np.clip(particles[:, d], min_values[d], max_values[d])
+        # Evaluate fitness of each particle
+        fitness_values = np.array([cost_func(p) for p in particles])
+
+        # Update best positions and fitness values
+        improved_indices = np.where(fitness_values < best_fitness)
+        best_positions[improved_indices] = particles[improved_indices]
+        best_fitness[improved_indices] = fitness_values[improved_indices]
+        if np.min(fitness_values) < swarm_best_fitness:
+            swarm_best_position = particles[np.argmin(fitness_values)]
+            swarm_best_fitness = np.min(fitness_values)
+
+        print(swarm_best_position, swarm_best_fitness)
+        if i % k == 0:  # Додано умову для відображення кожні k ітерацій
+                all_b = swarm_best_position
+                b_list = all_b.tolist()
+                b1, b2 = b_list[0], b_list[1]
+
+                x_values = np.linspace(0, max(x_f), 100)
+                y_values_func = func(x_values, b1, b2)
+
+                plt.scatter(x_f, y_f, label='Data')
+                plt.plot(x_values, y_values_func, color='red', label='Function: y = b1(1 - exp(-b2*x))')
+                plt.xlabel('x')
+                plt.ylabel('y')
+                plt.title('Regression plot')
+                plt.legend()
+                plt.grid(True)
+                plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+                plt.show()
+
+                x_values_loss = np.linspace(0, max(x_f), 14)
+                y_values_func_loss = func(x_values_loss, b1, b2)
+
+                mse_losses = mse_loss(y_values_func_loss.tolist(), y_f)
+                plt.plot(y_values_func_loss, mse_losses, label='MSE')
+                plt.xlabel('Predicted Value (y_pred)')
+                plt.ylabel('Loss')
+                plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+                plt.show()
+
+                YModelTrain = model(b_list, xTrain)
+
+                # Получение предсказанных значений для тестового набора данных
+                YModelTest = model(b_list, xTest)
+
+                plt.plot(YModelTrain, yTrain, '.r', markersize=15)
+                plt.plot(np.arange(min(yTrain), max(yTrain), 0.03), np.arange(min(yTrain), max(yTrain), 0.03))
+                plt.xlabel('YModelTrain')
+                plt.ylabel('yTrain (Real Data)')
+                plt.grid(True)
+                #print(YModelTrain)
+                plt.axis([min(YModelTrain), max(YModelTrain), min(yTrain), max(yTrain)])
+                plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+                plt.show()
+                # График для тестовых данных
+
+                plt.plot(YModelTest, yTest, '.', markersize=15)
+                plt.plot(np.arange(min(yTest), max(yTest), 0.03), np.arange(min(yTest), max(yTest), 0.03))
+                plt.xlabel('YModelTest')
+                plt.ylabel('yTest (Real Data)')
+                plt.grid(True)
+                plt.axis([min(YModelTest), max(YModelTest), min(yTest), max(yTest)])
+                plt.text(0.05, 0.95, f'Iteration: {i}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top')
+                plt.show()
+    # Return the best solution found by the PSO algorithm
+    return swarm_best_position, swarm_best_fitness
+
+# define population size
+pop_size = 100
+# define lower and upper bounds for every dimension
+bounds = asarray([(0, 100), (0, 1)])
+# define number of iterations
+iter = 101
+# define scale factor for mutation
+F = 0.5
+# define crossover rate for recombination
+cr = 0.7
+# define the frequency of updating plots
+k = 10  # кожні k ітерацій оновлюємо графіки
+ 
+# perform differential evolution
+solution = differential_evolution(pop_size, bounds, iter, F, cr, k)
+print(solution)
+
+# solution1=pso(obj)
+# print(solution1)
